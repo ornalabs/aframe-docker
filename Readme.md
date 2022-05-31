83c9a5c35946f5cb31994bd7158d75a996972c6b
rchovatiya88 <rchovatiya88@gmail.com>
2022-05-30 21:30:01 -0400

    int

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## ADD Docker with  webxr 
+
+## Step 1 
+
+  - Run this command `docker build -t aframe-docker . `  
+  - `docker run --name aframe-docker  -it debian`
+
+  
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello sdafsadfsafsfsfsa!'))
+app.get('/', (req, res) => res.send('yo earth!'))
 app.listen(3000, () => console.log('Server ready'))
